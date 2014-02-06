40b67fa559a7c56d69aae08bff6399ace855e9f9
Martin Lindblad <martin.lindblad@find-out.se>
2014-02-06 21:31:45 +0100

    copied project from node-tode

--- frontend/app/config.js
+++
@@ -1,8 +0,0 @@
-angular.module("config", [])
-
-.constant("config", {
-	"env": "development",
-	"version": "0.0.0"
-})
-
-;
--- frontend/app/js/order/showOrderedProductsFilter.js
+++
@@ -1,9 +0,0 @@
-app.filter('showOrderedProductsFilter', function() {
-    "use strict";
-
-    return function(products) {
-        return _.filter(products, function(product) {
-            return product.actualOrderAmount > 0;
-        });
-    };
-});
