3ccf3ece2b7cf1e722314103360a015a5bee7cd5
Martin Lindblad <martin.lindblad@find-out.se>
2014-02-12 22:43:39 +0100

    still in the middle

--- frontend/app/js/order/manageOrderController.js
+++ frontend/app/js/order/manageOrderController.js
@@ -1,8 +1,33 @@
-app.controller('ManageOrderController', function ($http) {
+app.controller('ManageOrderController', function ($http, $scope) {
     var self = this;
 
-    this.deleteOrderItem = function(orderItem) {
-        console.log(orderItem);
+    this.deleteOrder = function(order) {
+        for(var i = 0; i < self.orders.length; i++) {
+            var obj = self.orders[i];
+
+            if(_.isEqual(obj, order)) {
+                self.orders.splice(i, 1);
+            }
+        }
+    }
+
+    this.createOrder = function(name) {
+       self.orders.push({name : name, orderItems : {}});
+    }
+
+    this.createOrderItem = function(order, orderItem) {
+        order.orderItems.push({number: orderItem.number, name:orderItem.name, actualOrderAmount:orderItem.actualOrderAmount});
+    }
+
+    this.deleteOrderItem = function(order, number) {
+        console.log($scope);
+        for(var i = 0; i < order.orderItems.length; i++) {
+            var obj = order.orderItems[i];
+
+            if(number === obj.number) {
+                order.orderItems.splice(i, 1);
+            }
+        }
     }
 
     $http.get('/api/orders')
