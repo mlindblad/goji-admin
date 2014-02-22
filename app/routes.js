3e9a46da6d2a767dabe12d590c500eec4feeb97c
Martin Lindblad <martin.lindblad@find-out.se>
2014-02-23 00:58:24 +0100

    changed route for index.html

--- app/routes.js
+++ app/routes.js
@@ -92,7 +92,7 @@ module.exports = function(app) {
     });
 
 	// application -------------------------------------------------------------
-//	app.get('*', function(req, res) {
-//		res.sendfile('./frontend/app/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-//	});
+	app.get('*', function(req, res) {
+		res.sendfile('./frontend/app/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+	});
 };
